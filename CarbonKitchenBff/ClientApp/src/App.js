1c6638f41898fac2d65870924bf27cecb6989820
Paul DeVito <pdevito3@gmail.com>
2021-09-26 12:48:33 -0400

    react query and axios

--- CarbonKitchenBff/ClientApp/src/App.js
+++ CarbonKitchenBff/ClientApp/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Home } from './components/Home';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import './custom.css'
 
@@ -9,13 +11,10 @@ export default class App extends Component {
 
   render () {
     return (
-      // <Layout>
-        <>
-        <Route exact path='/' component={Home} />
-        {/*<Route path='/counter' component={Counter} />*/}
-        {/*<Route path='/fetch-data' component={FetchData} />*/}
-        </>
-      // </Layout>
+        <QueryClientProvider client={new QueryClient()}>
+            <Route exact path='/' component={Home} />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
   }
 }
