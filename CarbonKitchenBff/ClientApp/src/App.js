b8fc0e9f110ca86d36fa083952b37e1fd39fc1d2
Paul DeVito <pdevito3@gmail.com>
2021-09-26 08:51:26 -0400

    old react cleanup

--- CarbonKitchenBff/ClientApp/src/App.js
+++ CarbonKitchenBff/ClientApp/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 import './custom.css'
 
@@ -12,11 +9,13 @@ export default class App extends Component {
 
   render () {
     return (
-      <Layout>
+      // <Layout>
+        <>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+        {/*<Route path='/counter' component={Counter} />*/}
+        {/*<Route path='/fetch-data' component={FetchData} />*/}
+        </>
+      // </Layout>
     );
   }
 }
