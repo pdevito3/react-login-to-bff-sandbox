662fa338dd3e6fe51c2dd27cb802807452b50937
Paul DeVito <pdevito3@gmail.com>
2022-02-07 21:18:09 -0500

    tailwind v3

--- react-six/ClientApp/src/main.tsx
+++ react-six/ClientApp/src/main.tsx
@@ -1,7 +1,7 @@
+import '@/custom.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './custom.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
