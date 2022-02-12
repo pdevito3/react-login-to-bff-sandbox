972fdde2ef1fc8b04c51fb60292518fe7302a013
Paul DeVito <pdevito3@gmail.com>
2022-02-12 14:28:52 -0500

    missing bg update

--- react-six/ClientApp/src/components/Headers/PrivateHeader.tsx
+++ react-six/ClientApp/src/components/Headers/PrivateHeader.tsx
@@ -25,7 +25,7 @@ function PrivateHeader() {
 					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 						<button
 							type="button"
-							className="p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+							className="p-1 text-gray-400 bg-gray-900 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 						>
 							<span className="sr-only">View notifications</span>
 							<svg
